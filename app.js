4f7adede941389d6eac6a2ee8cf593af3be5eed6
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-04-15 10:18:49 +0200

    entry point 5

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', booksRouter);
+app.use('/books', booksRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
 module.exports = app;
